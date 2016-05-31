04400eaa6b866ffdab6f330ef1fe5c81af38328c
christian-bromann <contact@christian-bromann.com>
2016-05-30 23:50:20 -0700

    enable retry ability for step definitions

---
+++ test/fixtures/retry-step-definition.js
@@ -0,0 +1,55 @@
+var assert = require('assert')
+
+let retryTest = 0
+let lastCommand
+
+global.syncAsync = {}
+
+module.exports = function () {
+    this.Given(/^I go on the website "([^"]*)" that can fail one time$/, {retry: 1}, (url) => {
+        let res = browser.url(url)
+        if (retryTest !== 0) {
+            retryTest--
+            throw new Error('FLAKE!')
+        }
+        lastCommand = res
+    })
+
+    this.Given(/^I set retryTest to "([^"]*)"$/, (cnt) => {
+        retryTest = parseInt(cnt, 10)
+    })
+
+    this.Given(/^I go on the website "([^"]*)" the async way$/, {retry: 3}, function async (url) {
+        return browser.url(url).then((res) => {
+            if (retryTest !== 0) {
+                retryTest--
+                throw new Error('FLAKE!')
+            }
+            lastCommand = res
+        })
+    })
+
+    this.Then(/^should the title of the page be "([^"]*)"$/, {retry: 2}, (expectedTitle) => {
+        let title = browser.getTitle()
+        if (retryTest !== 0) {
+            retryTest--
+            throw new Error('FLAKE!')
+        }
+        assert.equal(title, expectedTitle)
+        lastCommand = 'getTitle'
+    })
+
+    this.Then(/^I click on link "([^"]*)" the async way$/, { retry: 2 }, function async (selector) {
+        if (retryTest !== 0) {
+            retryTest--
+            throw new Error('FLAKE!')
+        }
+        lastCommand = 'click'
+        return browser.click(selector)
+    })
+
+    this.Then(/^the last command should be "([^"]*)"$/, (expectedLastCommand) => {
+        assert.equal(expectedLastCommand, lastCommand)
+        lastCommand = null
+    })
+}
---
+++ test/retry.spec.js
@@ -0,0 +1,51 @@
+import { CucumberAdapter } from '../lib/adapter'
+
+const conf = {
+    cucumberOpts: {
+        timeout: 15000,
+        require: [__dirname + '/fixtures/retry-step-definition.js']
+    }
+}
+const feature = ['./test/fixtures/retry.feature']
+
+const NOOP = () => {}
+
+const WebdriverIO = class {}
+WebdriverIO.prototype = {
+    /**
+     * task of this command is to add 1 so we can have a simple demo test like
+     * browser.command(1).should.be.equal(2)
+     */
+    url: () => new Promise((r) => {
+        setTimeout(() => r('url'), 1000)
+    }),
+    click: () => new Promise((r) => {
+        setTimeout(() => r('click'), 1000)
+    }),
+    getTitle: (ms = 500) => new Promise((r) => {
+        setTimeout(() => r('Google'), 1000)
+    }),
+    pause: (ms = 500) => new Promise((r) => {
+        setTimeout(() => r('pause'), 1000)
+    })
+}
+
+process.send = NOOP
+
+let timeToExecute
+describe('CucumberAdapter', () => {
+    describe('executes step definitions async', () => {
+        before(async () => {
+            global.browser = new WebdriverIO()
+            const adapter = new CucumberAdapter(0, conf, feature, {})
+
+            const start = new Date().getTime();
+            (await adapter.run()).should.be.equal(0, 'actual test failed')
+            timeToExecute = new Date().getTime() - start
+        })
+
+        it('should take the expected amount of time to execute suite', () => {
+            timeToExecute.should.be.above(10000)
+        })
+    })
+})
